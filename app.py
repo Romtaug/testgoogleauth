c198abe8bc62626020005ed26ce6a80e590c77cb
Romtaug <87935987+Romtaug@users.noreply.github.com>
2026-04-09 04:04:08 +0200

    Remove email authorization check from app.py
    Removed email authorization check and related messages.

--- app.py
+++ app.py
@@ -6,12 +6,3 @@ if not st.user.is_logged_in:
 
 st.write("Connecté")
 st.write(st.user)
-
-EMAILS_AUTORISES = ["romtaug@gmail.com"]
-
-if st.user.email not in EMAILS_AUTORISES:
-    st.error(f"Accès non autorisé : {st.user.email}")
-    st.stop()
-
-st.title(f"Bonjour {st.user.name} 👋")
-st.write("✅ Accès autorisé")
