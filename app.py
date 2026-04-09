fda90d3f7afedbc0ba68465e8ed55f76739a88b2
Romtaug <87935987+Romtaug@users.noreply.github.com>
2026-04-09 04:09:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 
-if not st.user.is_logged_in:
-    st.button("Log in with Google", on_click=st.login, args=["google"])
-    st.stop()
+names = ["Rom"]
+usernames = ["romtaug"]
+passwords = ["motdepassefort"]
 
-st.write("Connecté")
-st.write(st.user)
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+authenticator = stauth.Authenticate(
+    dict(credentials=dict(usernames={
+        usernames[0]: dict(name=names[0], password=hashed_passwords[0])
+    })),
+    "app_cookie",
+    "une_signature_tres_longue_et_aleatoire",
+    cookie_expiry_days=30,
+)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status is False:
+    st.error("Identifiant ou mot de passe incorrect")
+elif authentication_status is None:
+    st.warning("Entrez vos identifiants")
+elif authentication_status:
+    authenticator.logout("Logout", "sidebar")
+    st.title(f"Bonjour {name} 👋")
+    st.write("✅ Accès autorisé")
